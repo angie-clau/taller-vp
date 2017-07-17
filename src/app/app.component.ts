35e32f76fe829379e8bbc207b9105d0888986777
Claudia Davila <claudia.davila@internetbrands.com>
2017-07-16 21:08:29 -0500

    wip: remove unused files. Add Boostrap to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'vp-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<div>hello Taller</div>',
 })
 export class AppComponent {
   title = 'vp';
